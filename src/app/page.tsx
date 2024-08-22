222a5c76b6dbf9c39be2d7da51b468bbf7ef373f
Zachary Shepherd <zacharyshepherd@outlook.com>
2024-08-21 23:21:43 -0600

    small updates 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,43 +18,8 @@ const navigation = {
   ],
   social: [
     {
-      name: 'Facebook',
-      href: '#',
-      icon: (
-        <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'Instagram',
-      href: '#',
-      icon: (
-        <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'X',
-      href: '#',
-      icon: (
-        <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
-          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-        </svg>
-      ),
-    },
-    {
       name: 'GitHub',
-      href: '#',
+      href: 'https://github.com/zacharyshepherd',
       icon: (
         <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
           <path
@@ -66,18 +31,30 @@ const navigation = {
       ),
     },
     {
-      name: 'YouTube',
-      href: '#',
+      name: 'Instagram',
+      href: 'https://www.instagram.com/zachary_shepherd/',
       icon: (
         <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
           <path
             fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
+            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
             clipRule="evenodd"
           />
         </svg>
       ),
     },
+    {
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/in/',
+      icon: (
+        <svg fill="currentColor" aria-hidden="true" className="h-6 w-6" viewBox="0 0 24 24">
+          <path
+            d="M20.447 20.452h-3.724v-5.569c0-1.327-.026-3.036-1.85-3.036-1.852 0-2.135 1.447-2.135 2.944v5.661H9.014V9h3.576v1.561h.051c.498-.947 1.712-1.946 3.523-1.946 3.76 0 4.454 2.475 4.454 5.694v6.143zM5.337 7.433c-1.2 0-2.172-.974-2.172-2.172 0-1.198.972-2.171 2.172-2.171 1.197 0 2.171.973 2.171 2.171 0 1.198-.974 2.172-2.171 2.172zm1.861 13.019H3.475V9h3.723v11.452zm14.912-20.452H2.892C1.297 0 0 1.297 0 2.892v18.217C0 22.703 1.297 24 2.892 24h18.217C22.703 24 24 22.703 24 21.108V2.892C24 1.297 22.703 0 21.108 0z"
+          />
+      </svg>
+      ),
+    },
+
   ],
 };
 
@@ -163,7 +140,7 @@ export default function Home() {
   return (
     <main className='max-w-7xl w-full px-8 mx-auto'>
       {/* Header */}
-      <header className='fixed w-full py-6 lg:px-8 m-auto top-0 left-0 z-50'>
+      <header className='fixed bg-slate-900 bg-opacity-90 backdrop-blur w-full py-6 lg:px-8 m-auto top-0 left-0 z-50'>
         <nav aria-label="Global" className="mx-auto flex items-center justify-between px-6">
           <div className="flex lg:flex-1">
             <button className="-m-1.5 p-1.5" onClick={() => {
@@ -173,8 +150,8 @@ export default function Home() {
               })
             }}>
               <span className="sr-only">Zachary Shepherd</span>
-              <svg xmlns="http://www.w3.org/2000/svg" className='h-10 w-auto' zoomAndPan="magnify" viewBox="0 0 75 74.999997" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><g/><clipPath id="00e7f4d56e"><path d="M 2.023438 2.15625 L 73.273438 2.15625 L 73.273438 73.40625 L 2.023438 73.40625 Z M 2.023438 2.15625 " clip-rule="nonzero"/></clipPath><clipPath id="0ace7073a7"><path d="M 8.023438 2.15625 L 67.121094 2.15625 C 70.4375 2.15625 73.121094 4.84375 73.121094 8.15625 L 73.121094 67.253906 C 73.121094 70.570312 70.4375 73.253906 67.121094 73.253906 L 8.023438 73.253906 C 4.710938 73.253906 2.023438 70.570312 2.023438 67.253906 L 2.023438 8.15625 C 2.023438 4.84375 4.710938 2.15625 8.023438 2.15625 Z M 8.023438 2.15625 " clip-rule="nonzero"/></clipPath></defs><g clip-path="url(#00e7f4d56e)"><g clip-path="url(#0ace7073a7)"><path stroke-linecap="butt" transform="matrix(0.75, 0, 0, 0.75, 2.025001, 2.157129)" fill="none" stroke-linejoin="miter" d="M 7.997916 -0.00117184 L 86.794797 -0.00117184 C 91.216672 -0.00117184 94.794798 3.582162 94.794798 7.998829 L 94.794798 86.79571 C 94.794798 91.217586 91.216672 94.795711 86.794797 94.795711 L 7.997916 94.795711 C 3.581249 94.795711 -0.00208511 91.217586 -0.00208511 86.79571 L -0.00208511 7.998829 C -0.00208511 3.582162 3.581249 -0.00117184 7.997916 -0.00117184 Z M 7.997916 -0.00117184 " stroke="#22d3ee" stroke-width="16" stroke-opacity="1" stroke-miterlimit="4"/></g></g><g fill="#22d3ee" fill-opacity="1"><g transform="translate(16.473825, 58.300005)"><g><path d="M 4.46875 0 L 4.46875 -7 L 25.21875 -32.6875 L 25.21875 -33 L 5.1875 -33 L 5.1875 -42.375 L 39.375 -42.375 L 39.375 -34.734375 L 19.890625 -9.6875 L 19.890625 -9.375 L 40.078125 -9.375 L 40.078125 0 Z M 4.46875 0 "/></g></g></g></svg>              
-            </button>        
+              <svg className='active' xmlns="http://www.w3.org/2000/svg" width="40" zoomAndPan="magnify" viewBox="0 0 75 74.999997" height="75" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><g></g><clipPath id="00e7f4d56e"><path d="M 2.023438 2.15625 L 73.273438 2.15625 L 73.273438 73.40625 L 2.023438 73.40625 Z M 2.023438 2.15625 " clip-rule="nonzero" className="svg-elem-1"></path></clipPath><clipPath id="0ace7073a7"><path d="M 8.023438 2.15625 L 67.121094 2.15625 C 70.4375 2.15625 73.121094 4.84375 73.121094 8.15625 L 73.121094 67.253906 C 73.121094 70.570312 70.4375 73.253906 67.121094 73.253906 L 8.023438 73.253906 C 4.710938 73.253906 2.023438 70.570312 2.023438 67.253906 L 2.023438 8.15625 C 2.023438 4.84375 4.710938 2.15625 8.023438 2.15625 Z M 8.023438 2.15625 " clip-rule="nonzero" className="svg-elem-2"></path></clipPath></defs><g clip-path="url(#00e7f4d56e)"><g clip-path="url(#0ace7073a7)"><path stroke-linecap="butt" transform="matrix(0.75, 0, 0, 0.75, 2.025001, 2.157129)" fill="none" stroke-linejoin="miter" d="M 7.997916 -0.00117184 L 86.794797 -0.00117184 C 91.216672 -0.00117184 94.794798 3.582162 94.794798 7.998829 L 94.794798 86.79571 C 94.794798 91.217586 91.216672 94.795711 86.794797 94.795711 L 7.997916 94.795711 C 3.581249 94.795711 -0.00208511 91.217586 -0.00208511 86.79571 L -0.00208511 7.998829 C -0.00208511 3.582162 3.581249 -0.00117184 7.997916 -0.00117184 Z M 7.997916 -0.00117184 " stroke="#22d3ee" stroke-width="16" stroke-opacity="1" stroke-miterlimit="4" className="svg-elem-3"></path></g></g><g fill="#22d3ee" fill-opacity="1"><g transform="translate(16.473825, 58.300005)"><g><path d="M 4.46875 0 L 4.46875 -7 L 25.21875 -32.6875 L 25.21875 -33 L 5.1875 -33 L 5.1875 -42.375 L 39.375 -42.375 L 39.375 -34.734375 L 19.890625 -9.6875 L 19.890625 -9.375 L 40.078125 -9.375 L 40.078125 0 Z M 4.46875 0 " className="svg-elem-4"></path></g></g></g></svg>            
+              </button>        
             </div>
             <div className='flex lg:gap-x-12'>
             <div className="hidden lg:flex lg:gap-x-12 items-center">
@@ -198,29 +175,29 @@ export default function Home() {
         </nav>
       </header>
 
-      {/* HERO */}
       <section id='hero' className="relative isolate flex h-screen">
-          <div className="w-full m-auto">
-            <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-4xl">
-              <p className='text-cyan-400 text-1xl font-mono'>Hello, my name is</p>
-              <h1 className="mt-6 text-4xl font-bold tracking-tight text-slate-200 sm:text-6xl">
-                Zachary Shepherd
-              </h1>
-              <h2 className='mt-4 text-4xl font-bold tracking-tight text-slate-400 sm:text-6xl'>I am a Software Engineer</h2>
-              <p className="mt-8 text-lg leading-8 text-slate-400">
-                I’m a software engineer at <a className='text-cyan-400' href='https://rcwilley.com' target='blank'>RC Willey</a>, specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on creating accessible, human-centered products.
-              </p>
-              <div className="mt-10 flex items-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md border-2 border-cyan-400 px-3.5 py-2.5 text-sm font-semibold text-slate-200 shadow-sm hover:bg-cyan-600 focus-visible:border focus-visible:border-2 focus-visible:border-spacing-2 focus-visible:border-cyan-600"
-                >
-                  View Resume                
-                </a>
-              </div>
+            <div className="w-full m-auto">
+                <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-4xl">
+                <p className='text-cyan-400 text-1xl font-mono'>Hello, my name is</p>
+                <h1 className="mt-6 text-4xl font-bold tracking-tight text-slate-200 sm:text-6xl">
+                    Zachary Shepherd
+                </h1>
+                <h2 className='mt-4 text-4xl font-bold tracking-tight text-slate-400 sm:text-6xl'>I am a Software Engineer</h2>
+                <p className="mt-8 text-lg leading-8 text-slate-400">
+                    I’m a software engineer at <a className='text-cyan-400' href='https://rcwilley.com' target='blank'>RC Willey</a>, specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on creating accessible, human-centered products.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                    <a
+                    href="#"
+                    className="rounded-md border-2 border-cyan-400 px-3.5 py-2.5 text-sm font-semibold text-slate-200 shadow-sm hover:bg-cyan-600 focus-visible:border focus-visible:border-2 focus-visible:border-spacing-2 focus-visible:border-cyan-600"
+                    >
+                    View Resume                
+                    </a>
+                </div>
+                </div>
             </div>
-          </div>
         </section>
+
 
         {/* ABOUT */}
           <section id='about' className="overflow-hidden py-20 sm:py-32">
@@ -362,19 +339,24 @@ export default function Home() {
 
         {/* FOOTER */}
         <footer>
-          <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+          <div className="mx-auto lg:block hidden max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
             <nav aria-label="Footer" className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12">
               {navigation.main.map((item) => (
                 <div key={item.name} className="pb-6">
-                  <a href={item.href} className="text-sm leading-6 text-slate-400 hover:text-cyan-400">
+                  <button key={item.name} className="text-sm target font-semibold leading-6 text-gray-300" onClick={() => {
+                  const elemnt = document.getElementById(item.href);
+                  elemnt?.scrollIntoView({
+                    behavior: 'smooth'
+                  })
+                }}>
                     {item.name}
-                  </a>
+                  </button>
                 </div>
               ))}
             </nav>
             <div className="mt-10 flex justify-center space-x-10">
               {navigation.social.map((item) => (
-                <a key={item.name} href={item.href} className="text-slate-400 hover:text-cyan-400">
+                <a key={item.name} href={item.href} target='_blank' className="text-slate-400 hover:text-cyan-400">
                   <span className="sr-only">{item.name}</span>
                   {item.icon}
                 </a>
